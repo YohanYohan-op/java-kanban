b22a9c9c34ca8ca9ea905a864665add50cf6d000
Слава <korolslava0@gmail.com>
2024-12-13 18:36:25 +0700

    Добавить неограниченную историю с помощью самописного Linked list

--- src/ru/yandex/javacource/korolyov/taskmanager/manager/DoublyLinkedList.java
+++ src/ru/yandex/javacource/korolyov/taskmanager/manager/DoublyLinkedList.java
@@ -89,16 +89,3 @@ public class DoublyLinkedList<T> {
         }
     }
 }
-
-class Node<T> {
-
-    public T data;
-    public Node<T> next;
-    public Node<T> prev;
-
-    public Node(T data) {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-    }
-}
