f8214d0511770d9d1b34c33b7d14f71743f8f05e
Слава <korolslava0@gmail.com>
2025-01-03 10:57:33 +0700

    Поправил ошибки

--- src/ru/yandex/javacource/korolyov/taskmanager/manager/Managers.java
+++ src/ru/yandex/javacource/korolyov/taskmanager/manager/Managers.java
@@ -2,7 +2,6 @@ package ru.yandex.javacource.korolyov.taskmanager.manager;
 
 import ru.yandex.javacource.korolyov.taskmanager.manager.inmemorymanaged.FileBackedTaskManager;
 import ru.yandex.javacource.korolyov.taskmanager.manager.inmemorymanaged.InMemoryHistoryManager;
-import ru.yandex.javacource.korolyov.taskmanager.manager.inmemorymanaged.InMemoryTaskManager;
 import ru.yandex.javacource.korolyov.taskmanager.manager.inmemorymanaged.interfaces.HistoryManager;
 import ru.yandex.javacource.korolyov.taskmanager.manager.inmemorymanaged.interfaces.TaskManager;
 
